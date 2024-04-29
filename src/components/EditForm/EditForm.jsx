6a0184712ad80fd1d44a1029062ad263c3d95170
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-04-29 08:28:15 +0300

    change todo

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -1,29 +1,57 @@
 import { RiSaveLine } from 'react-icons/ri';
 import { MdOutlineCancel } from 'react-icons/md';
 import Modal from 'react-modal';
-import style from './EditForm.module.css'
+import style from './EditForm.module.css';
+import { useState } from 'react';
 
-export const EditForm = ({isOpen, closeModal, todoData}) => {
+export const EditForm = ({ isOpen, closeModal, todos, onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const onChange = e => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    onSubmit({
+      text: e.target.elements.text.value,
+      id: Date.now(),
+    });
+
+    setQuery('');
+  };
+
   return (
     <Modal isOpen={isOpen} onRequestClose={closeModal}>
-      <form className={style.form}>
-  <button className={style.submitButton} type="submit">
-    <RiSaveLine color="green" size="16px" />
-  </button>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <button
+          className={style.submitButton}
+          type="submit"
+          // onClick={() => closeModal()}
+        >
+          <RiSaveLine color="green" size="16px" />
+        </button>
 
-  <button className={style.editButton} type="button">
-    <MdOutlineCancel color="red" size="16px" />
-  </button>
+        <button
+          className={style.editButton}
+          type="button"
+          onClick={() => closeModal()}
+        >
+          <MdOutlineCancel color="red" size="16px" />
+        </button>
 
-  <input
-    className={style.input}
-    placeholder="What do you want to write?"
-    name="text"
-    required
-    // defaultValue={defaultValue}
-    autoFocus
-  />
-</form>
+        <input
+          className={style.input}
+          placeholder="What do you want to write?"
+          name="text"
+          required
+          defaultValue={todos.text}
+          autoFocus
+          value={query}
+          onChange={onChange}
+        />
+      </form>
     </Modal>
-  )
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,54 +1,69 @@
-import FormToDo from "../components/FormToDo/FormToDo";
+import FormToDo from '../components/FormToDo/FormToDo';
 import { Text } from 'components';
 import { useEffect, useState } from 'react';
-import {TodoList} from '../components/TodoList/TodoList'
-import {EditForm} from '../components/EditForm/EditForm'
+import { TodoList } from '../components/TodoList/TodoList';
+import { EditForm } from '../components/EditForm/EditForm';
 import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
 export const Todos = () => {
-  const [todos, setTodos] = useState(()=>{
+  const [todos, setTodos] = useState(() => {
     const savedTodos = window.localStorage.getItem('todos list');
     if (savedTodos !== null) {
-      return JSON.parse(savedTodos)
-    } return ([])
+      return JSON.parse(savedTodos);
+    }
+    return [];
   });
   const [modalIsOpen, setIsOpen] = useState(false);
   const [selectedTodo, setSelectedTodo] = useState(null);
 
-useEffect(()=> {
-  window.localStorage.setItem('todos list', JSON.stringify(todos))
-}, [todos])
+  useEffect(() => {
+    window.localStorage.setItem('todos list', JSON.stringify(todos));
+  }, [todos]);
 
-  const addTodo = (newTodo) => {
+  const addTodo = newTodo => {
     setTodos(prevTodos => {
-      return [...prevTodos, newTodo]
-    })
-  }
+      return [...prevTodos, newTodo];
+    });
+  };
 
-const deleteTodo = (todoId) => {
-  setTodos(prevTodos => {
-    return prevTodos.filter((todo) => todo.id !== todoId)
-  })
-}
-const openModal= () => {
-      setIsOpen(true)      
-     }
-const closeModal = () => {
-      setIsOpen(false)
-    }
+  const deleteTodo = todoId => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
-const changeTodo = (todoId) => {
-  const todoToChange = todos.find(todo => todo.id === todoId);
+  const changeTodo = todoId => {
+    const todoToChange = todos.find(todo => todo.id === todoId);
     setSelectedTodo(todoToChange);
-  openModal()
-}
+    openModal();
+  };
 
-
-  return (<div>
-    {todos.length === 0 && <Text textAlign="center">There are no any todos ...</Text>}
-    <FormToDo onSubmit={addTodo} />
-  <TodoList todos={todos} onDelete={deleteTodo} onChange={changeTodo}/>
-<EditForm isOpen={modalIsOpen} closeModal={closeModal} todoData={selectedTodo}/>
-  </div>)
+  return (
+    <div>
+      {todos.length === 0 && (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
+      <FormToDo onSubmit={addTodo} />
+      <TodoList todos={todos} onDelete={deleteTodo} onChange={changeTodo} />
+      <EditForm
+        isOpen={modalIsOpen}
+        closeModal={closeModal}
+        todos={todos}
+        onSubmit={editedTodo => {
+          const updatedTodos = todos.map(todo =>
+            todo.id === selectedTodo.id ? editedTodo : todo,
+          );
+          setTodos(updatedTodos);
+          closeModal();
+        }}
+      />
+    </div>
+  );
 };
